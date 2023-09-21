c32a2bd8715ce3fd1e5bafed635d5494531e69d5
Laboratorio 109 Pc-08 <lab-109-inf-08>
2023-09-21 18:57:07 -0400

    first commit

---
+++ src/SimpleInventario.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class SimpleInventario {
+    public static void agregarArticulo() {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Agregar un articulo:  ");
+    }
+}
